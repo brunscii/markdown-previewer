1da186825657525d2eaf0caf68c3e4f151d5ee91
Chris Carlin <brunscii@gmail.com>
2022-11-02 16:44:13 -0400

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class MarkdownBox extends React.Component {
+    static defualtProps = {
+        input: ''
+    }
+    constructor(props){
+        super(props)
+        this.state = {
+            input: props.input
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.getInputValue = this.getInputValue.bind(this);
+    };
+
+    handleChange(e){
+        this.setState({input: e.target.value});
+        console.log('yo')
+    }
+
+    getInputValue(){
+        return this.state.input;
+    }
+
+    render(){
+        return (
+            <>
+            <h1>Hello</h1>
+            <input id='markdown-text' value={this.state.input} onChange={this.handleChange}></input>
+            </>
+        )
+    }
+}
+
+class PreviewBox extends React.Component{
+    static defualtProps = {
+        input: ''
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: props.input
+        }
+        this.conv = this.conv.bind(this);
+    }
+    conv(inp){
+        //add code to fetch conversion from api
+        return 0;
+    }
+    render(){
+        
+        return(
+            <div className='htmlBox' style = {{display: 'flex',width: 200, height: 200, border: '1px solid black', backgroundColor: '#333', color:'#ccc'}}>
+                <p>{this.state.input}</p>
+            </div>
+            )
+    }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <MarkdownBox input='Hello, world!'/>
+        <PreviewBox  input='Hello HTML'/>
+    </React.StrictMode>
+)
